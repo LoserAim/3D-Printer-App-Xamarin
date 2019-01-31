a606c45752d08b53bb17998cf9c88de7cb388c0f
Bradley Bergstrom <35245874+BradBergstrom@users.noreply.github.com>
2019-01-30 16:36:18 -0800

    Fixed Project Namespace Error

---
+++ PrintQue/PrintQue/PrintQue/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrintQue
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isUsernameEmpty = string.IsNullOrEmpty(userNameEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(userPasswordEntry.Text);
+
+            if (isUsernameEmpty || isPasswordEmpty)
+            {
+                //then show error
+
+            }
+            else
+            {
+                //then try to log in user
+
+            }
+
+        }
+    }
+}
