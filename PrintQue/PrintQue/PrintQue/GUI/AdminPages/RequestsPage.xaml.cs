7ffa80b18568c6f2a86e4e81b2624b4744d1b97c
Andrew Doser <38057761+LoserAim@users.noreply.github.com>
2019-02-15 19:02:29 -0800

    IMplementation of data binding

--- PrintQue/PrintQue/PrintQue/GUI/AdminPages/RequestsPage.xaml.cs
+++ PrintQue/PrintQue/PrintQue/GUI/AdminPages/RequestsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PrintQue.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,77 @@ namespace PrintQue
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RequestsPage : ContentPage
 	{
+        public int ItemSelected;
 		public RequestsPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Request>();
+                var requests = conn.Table<Request>().ToList();
+                if (requests.Count < 1)
+                {
+                    var TestRequest = new Request()
+                    {
+                        ProjectName = "BubbyHitMeUp",
+                        UserID = 1,
+                        PrinterID = 1,
+
+                    };
+                    conn.Insert(TestRequest);
+                }
+                
+
+            }
+        }
+
+
+
+        private void ClickedMenuDeny(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+            }
+            catch
+            {
+
+            }
+        }
+        private void ClickedMenuDetails(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+            }
+            catch
+            {
+
+            }
+        }
+        protected override void OnAppearing()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Request>();
+                var requests = conn.Table<Request>().ToList();
+                RequestListView.ItemsSource = requests;
+
+            }
+        }
+
+        public void OnMore(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("More Context Action", mi.CommandParameter + " more context action", "OK");
+        }
+
+        public void OnDelete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("Delete Context Action", mi.CommandParameter + " delete context action", "OK");
+        }
+    }
 }
