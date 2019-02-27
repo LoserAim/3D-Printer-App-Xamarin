17936b1856ac880ea463253717df3d9cfe4e5d00
Znowy <jknudsen99@gmail.com>
2019-02-26 18:42:58 -0800

    Started Chat Integration

---
+++ PrintQue/PrintQue/PrintQue/Models/ChatViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrintQue.Models
+{
+    public class ChatViewModel
+    {
+        HubConnection hubConnection;
+        public ChatViewModel()
+        {
+            // localhost for UWP/iOS or special IP for Android
+            var ip = "localhost";
+            if (Device.RuntimePlatform == Device.Android)
+                ip = "10.0.2.2";
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl($"http://{ip}:5000/chatHub")
+                .Build();
+        }
+
+        async Task Connect()
+        {
+            try
+            {
+                await hubConnection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                // Something has gone wrong
+            }
+        }
+
+        async Task SendMessage(string user, string message)
+        {
+            try
+            {
+                await hubConnection.InvokeAsync("SendMessage", user, message);
+            }
+            catch (Exception ex)
+            {
+                // send failed
+            }
+        }
+    }
+}
