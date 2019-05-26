1ac3246ab0bc6ff638a1efbec36375ddf8a0eaef
Andrew Doser <38057761+LoserAim@users.noreply.github.com>
2019-05-26 00:00:13 -0700

    Revert "CustomRenderer absolete"
    This reverts commit 255b729780cff9b52db069f31d03c454e9d6e899.

--- PrintQue/PrintQue/PrintQue.Android/MainActivity.cs
+++ PrintQue/PrintQue/PrintQue.Android/MainActivity.cs
@@ -7,7 +7,6 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using System.IO;
-using Plugin.CurrentActivity;
 
 namespace PrintQue.Droid
 {
@@ -22,7 +21,6 @@ namespace PrintQue.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             string DbName = "printerque_db.sqlite";
             string FolderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             string FullPath = Path.Combine(FolderPath, DbName);
