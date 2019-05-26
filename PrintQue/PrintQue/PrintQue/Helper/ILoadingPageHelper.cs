02212ba5f815d8e8351d664eda2d906ee72902f3
Andrew Doser <38057761+LoserAim@users.noreply.github.com>
2019-05-25 23:31:40 -0700

    sucks

---
+++ PrintQue/PrintQue/PrintQue/Helper/ILoadingPageHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PrintQue.Helper
+{
+    public interface ILoadingPageHelper
+    {
+        void InitLoadingPage
+              (ContentPage loadingIndicatorPage = null);
+
+        void ShowLoadingPage();
+
+        void HideLoadingPage();
+    }
+}
---
+++ PrintQue/PrintQue/PrintQue/Widgets/LoadingPageWidget/LoadingIndicatorLoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrintQue.Widgets.LoadingPageWidget
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoadingIndicatorLoginPage : ContentPage
+	{
+		public LoadingIndicatorLoginPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
